8edca58f61b8e545bd3806e249e136a45ffe40ef
huntj88 <huntj88@gmail.com>
2020-03-16 23:43:30 -0500

    cleanup, logging

--- src/main/kotlin/me/jameshunt/bplustree/LeafNeighborAccess.kt
+++ src/main/kotlin/me/jameshunt/bplustree/LeafNeighborAccess.kt
@@ -9,20 +9,24 @@ class LeafNeighborAccess {
     private var onRight: LeafNode<*, *>? = null
 
     fun lockLeftWrite() {
+//        println("locking left $this, with thread: ${Thread.currentThread()}")
         onLeft?.rwLock?.lockWrite()
+//        println("LOCKED left $this, with thread: ${Thread.currentThread()}")
     }
 
     fun lockRightWrite() {
+//        println("locking right $this, with thread: ${Thread.currentThread()}")
         onRight?.rwLock?.lockWrite()
+//        println("LOCKED right $this, with thread: ${Thread.currentThread()}")
     }
 
     fun unlockLeftWrite() {
-        access.unlock()
+//        println("unlocking left $this, with thread: ${Thread.currentThread()}")
         onLeft?.rwLock?.unlockWrite()
     }
 
     fun unlockRightWrite() {
-        access.unlock()
+//        println("unlocking right $this, with thread: ${Thread.currentThread()}")
         onRight?.rwLock?.unlockWrite()
     }
 
